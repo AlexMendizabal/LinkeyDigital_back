22d4c16c5013b020f9978b26ca677d7d7d519449
Augusto Nicolas <pepe.nico5@hotmail.com>
2023-05-30 09:17:38 -0400

    change settings

--- soyyo_api/settings.py
+++ soyyo_api/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'soyyo',
-        'USER': 'loza',
-        'PASSWORD': '1234',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
