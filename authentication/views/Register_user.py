63b65ed95cfe25e64ac6f3e6c3b848923fd7e2d0
Augusto Nicolas <pepe.nico5@hotmail.com>
2023-08-25 10:49:02 -0400

    Add WebHook
    Se agrego las funciones del web hook, que incluyen tambien las de email en contact

--- authentication/views/Register_user.py
+++ authentication/views/Register_user.py
@@ -33,7 +33,7 @@ class CreateUserThread(threading.Thread):
                 username = email.split('@')[0]
                 User = get_user_model()
                 print(self.licencia_id)
-                user = User.objects.create(uid=uid, email=email, username=username, licencia_id_id = self.licencia_id)
+                user = User.objects.create(uid=uid, email=email, username=username, licencia_id_id=self.licencia_id)
                 with self.lock:
                     self.corrects.append({"email": self.email})
             except Exception as e:
@@ -47,73 +47,60 @@ class CreateUserThread(threading.Thread):
                 self.errors.append({"email": self.email, "error": str(e)})
             raise e
 
+def create_users_in_threads(cant, correo, licencia_id, user_id=None):
+    if not licencia_id and user_id:
+        try:
+            user = CustomerUser.objects.get(id=user_id)
+        except Exception as e:
+            user = get_object_or_404(CustomerUser, username=user_id)
+        licencia_id = user.licencia_id
+
+    if not correo:
+        correo = "usuario"
+
+    User = get_user_model()
+    users = User.objects.filter(email__startswith=correo, email__endswith='@soyyo.digital')
+
+    numbers = [int(re.search(re.escape(correo) + r'-(\d+)@', user.email).group(1)) for user in users if re.search(re.escape(correo) + r'-(\d+)@', user.email)]
+
+    max_number = max(numbers) if numbers else 0
+
+    threads = []
+    errors = []
+    corrects = []
+    lock = Lock()
+
+    for i in range(1, cant + 1):
+        email = f"{correo}-{max_number + i}@soyyo.digital"
+        password = "soyyo.digital"
+        thread = CreateUserThread(email=email, password=password, errors=errors, lock=lock, corrects=corrects, licencia_id=licencia_id)
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    return errors, corrects
 
 class CreateALotOfUsers(APIView):
-
     def post(self, request):
         if not request.user.is_superuser:
             raise TokenNotFound()
-        # cantidad de usuarios a crear (solo admite 50)
-        cant = request.data.get("cant", None)
-        # nombre con el que iniciar el correo 
+
+        cant = int(request.data.get("cant", 0))
         correo = request.data.get("correo", None)
-        # licencia a la que agregar el usuario
-        licencia_id = request.data.get("licencia", None)
-        # se puede tambien mandar el user id del que se quiere extraer la licencia para no buscar la licencia en el front 
+        licencia_id = int(request.data.get("licencia", 0))
         user_id = request.data.get("user_id", None)
 
-        if not cant:
-            return Response({"success": False}, status=status.HTTP_400_BAD_REQUEST)
-        if not licencia_id and not user_id:
+        if not cant or (not licencia_id and not user_id):
             return Response({"success": False}, status=status.HTTP_400_BAD_REQUEST)
         
-        cant = int(cant)
         if cant > 50:
             cant = 50
 
-        if not licencia_id and user_id:
-            # busca por id, si no lo encuentra lo busca por username
-            try:
-                user = CustomerUser.objects.get(id=user_id)
-            except Exception as e:
-                user = get_object_or_404(CustomerUser, username=user_id)
-                    
-            licencia_id = user.licencia_id
-        if not correo:
-            correo = "usuario"
-        
-        licencia_id = int(licencia_id)
-        
-        User = get_user_model()
-        users = User.objects.filter(email__startswith=correo, email__endswith='@soyyo.digital')
-
-        numbers = []
-        for user in users:
-            email = user.email
-            regex_pattern = re.escape(correo) + r'-(\d+)@'
-            match = re.search(regex_pattern, email)
-            if match:
-                number = int(match.group(1))
-                numbers.append(number)
-
-        max_number = max(numbers) if numbers else 0
-
-        threads = []
-        errors = []
-        corrects = []
-        lock = Lock()
-        for i in range(1, cant+1):
-            email = str(correo) + "-" + str(max_number + i) + "@soyyo.digital"
-            password = "soyyo.digital"
-            thread = CreateUserThread(email=email, password=password, errors=errors, lock=lock, corrects=corrects, licencia_id=licencia_id)
-            thread.start()
-            threads.append(thread)
-
-        # Esperar a que todos los hilos terminen su ejecución
-        for thread in threads:
-            thread.join()
+        errors, corrects = create_users_in_threads(cant, correo, licencia_id, user_id)
 
         if errors:
             return Response({"success": True, "errors": errors, "corrects": corrects}, status=status.HTTP_200_OK)
 
-        return Response({"success": True, "corrects" : corrects}, status=status.HTTP_200_OK)
+        return Response({"success": True, "corrects": corrects}, status=status.HTTP_200_OK)
--- pay/views/__init__.py
+++ pay/views/__init__.py
@@ -1,2 +1,3 @@
 from .solicitudViewset import SolicitudViewSet
 from .consultaViewset import ConsultaViewSet, ConsultaExtendViewSet
+from .webhookConsulta import webhook
